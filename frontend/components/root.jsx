9759f2123a22c632092c5c62a41a0e07a93014a7
Kevin Dam <kevinddam@gmail.com>
2017-04-19 11:27:13 -0400

    fixed routes to redirect to login on root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory, IndexRedirect} from 'react-router';
 import App from './app';
 import AuthFormContainer from './auth/auth_form_container';
+
+
 
 const Root = ({ store }) => (
   <Provider store={ store }>
@@ -10,6 +12,7 @@ const Root = ({ store }) => (
       <Route path="/login" component={ AuthFormContainer } />
       <Route path="/signup" component={ AuthFormContainer } />
       <Route path="/" component={ App }>
+        <IndexRedirect to="/login" />
       </Route>
     </Router>
   </Provider>
