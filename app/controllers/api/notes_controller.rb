a04bc2d63b59344ae3fef2154d8ef42bff06f6bd
Kevin Dam <kevinddam@gmail.com>
2017-04-20 10:23:29 -0400

    fixed account creation

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -12,9 +12,21 @@ class Api::NotesController < ApplicationController
 
   def create
     @note = Note.new(note_params)
+    if @note.save
+      render 'api/notes/show'
+    else
+      render json: ["Invalid note criteria!"], status: 422
+    end
   end
 
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
 
+    else
+
+    end
+  end
 
   private
 
